0bd17794b4a06dc0bca102972fc5cc267f6182a4
Ankan Das <140325464+evuedeveloper@users.noreply.github.com>
2024-08-17 10:34:05 +0530

    render specific changes

--- server/config/db.js
+++ server/config/db.js
@@ -1,16 +1,18 @@
 const mongoose = require('mongoose');
+
 const connectDB = async () => {
-  
   try {
     mongoose.set('strictQuery', false);
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000, // Timeout after 5 seconds instead of 10
+    });
     console.log(`Database Connected: ${conn.connection.host}`);
-  } 
-  
-  catch (error) {
-    console.log(error);
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    process.exit(1); // Exit process with failure
   }
-
-}
+};
 
 module.exports = connectDB;
